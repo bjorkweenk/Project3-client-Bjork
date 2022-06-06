dcb695f38e1f80cfcc6749f9353b98f3fe646da6
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-06-06 12:39:55 +0200

    css

--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -4,6 +4,7 @@ import authService from "../../services/auth.service"
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/auth.context'
 import { MessageContext } from '../../context/message.context'
+import './Login.css'
 
 const Loginform = () => {
 
@@ -26,7 +27,7 @@ const Loginform = () => {
                 storeToken(data.authToken)
                 authenticateUser()
                 showMessage('Welcome!! Find your new favourite restaurant!!')
-                navigate('/')
+                navigate('/home')
             })
             .catch(err => console.log("Unable to login: ", err))
     }
@@ -38,24 +39,36 @@ const Loginform = () => {
 
     const { password, email } = loginData
 
-    
+
 
     return (
 
-        <Form onSubmit={handleSubmit}>
+        <>
+            <div class="container">
+                <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" width={400} height={400} />
+                <img src="https://files.slack.com/files-pri/T02CQ4EN4-F03J71R9KQE/image_from_ios.jpg"/>
+                <div class="text">
+                </div>
+                
+            </div>
 
-            <Form.Group className="login" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" onChange={handleInputChange} name="email" value={email} />
-            </Form.Group>
+            <Form onSubmit={handleSubmit}>
 
-            <Form.Group className="login" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" onChange={handleInputChange} name="password" value={password} />
-            </Form.Group>
+                <Form.Group className="login" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" onChange={handleInputChange} name="email" value={email} />
+                </Form.Group>
 
-            <Button variant="dark" type="submit">Log in</Button>
-        </Form>
+                <Form.Group className="login" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" onChange={handleInputChange} name="password" value={password} />
+                </Form.Group>
+
+                <Button variant="dark" type="submit">Log in</Button>
+            </Form>
+
+
+        </>
 
     )
 }
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -8,8 +8,6 @@ const LoginPage = () => {
         <Container>
             <Row>
                 <Col md={{ span: 6, offset: 3 }}>
-                    <h1>Login</h1>
-                    <hr />
                     <Loginform />
 
                 </Col>
